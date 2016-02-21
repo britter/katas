33f754be00150b60a373d4506895f4f997afffbd
Benedikt Ritter <beneritter@gmail.com>
2016-02-21 16:32:46 +0100

    Remove Backstage pass code from handleItem method

--- src/main/scala/de/britter/gildedrose/GildedRose.scala
+++ src/main/scala/de/britter/gildedrose/GildedRose.scala
@@ -50,29 +50,15 @@ class GildedRose(val items: Item*) {
   }
 
   private def handleItem(item: Item) = {
-    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
-      if (item.sellIn > 10) {
-        item.quality = item.quality + 1
-      } else if (item.sellIn <= 10 && item.sellIn > 5) {
-        item.quality = item.quality + 2
-      } else if (item.sellIn <= 5) {
-        item.quality = item.quality + 3
-      }
-    } else {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1
-      }
+    if (item.quality > 0) {
+      item.quality = item.quality - 1
     }
 
     item.sellIn = item.sellIn - 1
 
     if (item.sellIn < 0) {
-      if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        item.quality = 0
-      } else {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
+      if (item.quality > 0) {
+        item.quality = item.quality - 1
       }
     }
   }
