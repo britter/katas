2ea5f1e403f0d172f594c2f6d43e911edfbc91c9
Benedikt Ritter <beneritter@gmail.com>
2016-02-21 16:10:31 +0100

    Create block for backstage pass rules

--- src/main/scala/de/britter/gildedrose/GildedRose.scala
+++ src/main/scala/de/britter/gildedrose/GildedRose.scala
@@ -21,18 +21,17 @@ class GildedRose(val items: Item*) {
   def updateQuality() {
     for (item <- items) {
       if (item.name.equals("Aged Brie")
-        || item.name.equals("Backstage passes to a TAFKAL80ETC concert")
         || item.name.equals("Sulfuras, Hand of Ragnaros")) {
         if (item.quality < 50) {
           item.quality = item.quality + 1
-
-          if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn <= 10 && item.sellIn > 5) {
-              item.quality = item.quality + 1
-            } else if (item.sellIn <= 5) {
-              item.quality = item.quality + 2
-            }
-          }
+        }
+      } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
+        if (item.sellIn > 10) {
+          item.quality = item.quality + 1
+        } else if (item.sellIn <= 10 && item.sellIn > 5) {
+          item.quality = item.quality + 2
+        } else if (item.sellIn <= 5) {
+          item.quality = item.quality + 3
         }
       } else {
         if (item.quality > 0) {
