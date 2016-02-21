a96eeff82ed93deec8d2a025212f3d7322179457
Benedikt Ritter <beneritter@gmail.com>
2016-02-21 13:41:28 +0100

    Import Gilded Rose kata from https://github.com/emilybache/GildedRose-Refactoring-Kata

---
+++ src/main/scala/de/britter/gildedrose/GildedRose.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2016 Benedikt Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.britter.gildedrose
+
+class GildedRose(val items: Array[Item]) {
+
+  def updateQuality() {
+    for (i <- 0 until items.length) {
+      if (!items(i).name.equals("Aged Brie")
+        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (items(i).quality > 0) {
+          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+            items(i).quality = items(i).quality - 1
+          }
+        }
+      } else {
+        if (items(i).quality < 50) {
+          items(i).quality = items(i).quality + 1
+
+          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (items(i).sellIn < 11) {
+              if (items(i).quality < 50) {
+                items(i).quality = items(i).quality + 1
+              }
+            }
+
+            if (items(i).sellIn < 6) {
+              if (items(i).quality < 50) {
+                items(i).quality = items(i).quality + 1
+              }
+            }
+          }
+        }
+      }
+
+      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+        items(i).sellIn = items(i).sellIn - 1
+      }
+
+      if (items(i).sellIn < 0) {
+        if (!items(i).name.equals("Aged Brie")) {
+          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (items(i).quality > 0) {
+              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+                items(i).quality = items(i).quality - 1
+              }
+            }
+          } else {
+            items(i).quality = items(i).quality - items(i).quality
+          }
+        } else {
+          if (items(i).quality < 50) {
+            items(i).quality = items(i).quality + 1
+          }
+        }
+      }
+    }
+  }
+}
