10c1bb3a0618374035ef990dae9ad30f13525189
Benedikt Ritter <beneritter@gmail.com>
2016-02-21 16:00:08 +0100

    Remove redundant if checks

--- src/main/scala/de/britter/gildedrose/GildedRose.scala
+++ src/main/scala/de/britter/gildedrose/GildedRose.scala
@@ -28,15 +28,11 @@ class GildedRose(val items: Item*) {
 
           if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+              item.quality = item.quality + 1
             }
 
             if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+              item.quality = item.quality + 1
             }
           }
         }
