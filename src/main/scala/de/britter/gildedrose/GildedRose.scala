99139653b9203f6948c0acfb6ac04c09f3bd601e
Benedikt Ritter <beneritter@gmail.com>
2016-02-21 16:23:34 +0100

    Remove Aged Brie code from handleItem

--- src/main/scala/de/britter/gildedrose/GildedRose.scala
+++ src/main/scala/de/britter/gildedrose/GildedRose.scala
@@ -24,7 +24,7 @@ class GildedRose(val items: Item*) {
         item.sellIn = item.sellIn - 1
         if (item.quality < 50) {
           item.quality = item.quality + 1
-          if(item.sellIn < 0) {
+          if (item.sellIn < 0) {
             item.quality = item.quality + 1
           }
         }
@@ -35,11 +35,7 @@ class GildedRose(val items: Item*) {
   }
 
   private def handleItem(item: Item) = {
-    if (item.name.equals("Aged Brie")) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
-      }
-    } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
+    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
       if (item.sellIn > 10) {
         item.quality = item.quality + 1
       } else if (item.sellIn <= 10 && item.sellIn > 5) {
@@ -56,17 +52,11 @@ class GildedRose(val items: Item*) {
     item.sellIn = item.sellIn - 1
 
     if (item.sellIn < 0) {
-      if (item.name.equals("Aged Brie")) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+      if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        item.quality = 0
       } else {
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-          item.quality = 0
-        } else {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1
-          }
+        if (item.quality > 0) {
+          item.quality = item.quality - 1
         }
       }
     }
