46cf26ea90681e3f56831c8e0e8c6c3e3e6cdb5f
Benedikt Ritter <beneritter@gmail.com>
2016-02-21 16:37:18 +0100

    Refactor handling of regular items

--- src/main/scala/de/britter/gildedrose/GildedRose.scala
+++ src/main/scala/de/britter/gildedrose/GildedRose.scala
@@ -56,15 +56,12 @@ class GildedRose(val items: Item*) {
   }
 
   private def handleItem(item: Item) = {
-    if (item.quality > 0) {
-      item.quality = item.quality - 1
-    }
-
     item.sellIn = item.sellIn - 1
-
-    if (item.sellIn < 0) {
-      if (item.quality > 0) {
+    if (item.quality > 0) {
+      if(item.sellIn > 0) {
         item.quality = item.quality - 1
+      } else {
+        item.quality = item.quality - 2
       }
     }
   }
